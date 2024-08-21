0c3f457eef485cd87d42892f05b61b305b4bdc9f
giftthedeveloper <giftjeremiah001@gmail.com>
2024-08-21 20:42:18 +0100

    refact: added to allowed host list

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // const configService = app.get(ConfigService);
   // const allowedOrigin = process.env.ALLOWED_HOSTS
-
+  // const allowedOrigins = configService.get<string>('ALLOWED_HOSTS')?.split(',');
   app.enableCors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://5173-giftthedeve-zmregistrat-emivk6mc6b6.ws-eu115.gitpod.io/'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://5173-giftthedeve-zmregistrat-emivk6mc6b6.ws-eu115.gitpod.io'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, 
   });
